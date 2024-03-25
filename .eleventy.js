a61b014fbceaae7489d082912d3fafefca3ec713
Carlos Goce <carlosgoce@gmail.com>
2024-03-25 22:42:09 +0100

    Primera versión

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+	eleventyConfig.addPassthroughCopy("assets/bundle.css");
+	eleventyConfig.addPassthroughCopy("assets/rastrexando-circular.png");
+	eleventyConfig.addPassthroughCopy("assets/rastrexando-horizontal.png");
 };
