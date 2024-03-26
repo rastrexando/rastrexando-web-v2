be6b8bf3b58fb7aa15bd68d10d5576f6bb0ed66a
Carlos Goce <carlosgoce@gmail.com>
2024-03-26 01:15:29 +0100

    Añade ejemplo de piñeiro y mejores estilos

--- .eleventy.js
+++ .eleventy.js
@@ -4,4 +4,5 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("assets/bundle.css");
 	eleventyConfig.addPassthroughCopy("assets/rastrexando-circular.png");
 	eleventyConfig.addPassthroughCopy("assets/rastrexando-horizontal.png");
+	eleventyConfig.addPassthroughCopy("assets/images/");
 };
