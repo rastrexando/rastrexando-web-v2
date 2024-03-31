577d8f0be73a75ca195ce77bbe127fed953b1a7f
Carlos Goce <carlosgoce@gmail.com>
2024-04-01 00:08:52 +0200

    feat: Añade favicons

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
   eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
-	eleventyConfig.addPassthroughCopy("assets/bundle.css");
-	eleventyConfig.addPassthroughCopy("assets/rastrexando-circular.png");
-	eleventyConfig.addPassthroughCopy("assets/rastrexando-horizontal.png");
-	eleventyConfig.addPassthroughCopy("assets/landing.jpg");
-	eleventyConfig.addPassthroughCopy("assets/images/");
-	eleventyConfig.addPassthroughCopy("assets/foundation-icons/");
+	eleventyConfig.addPassthroughCopy("assets/");
 	eleventyConfig.addPassthroughCopy("CNAME");
 };
