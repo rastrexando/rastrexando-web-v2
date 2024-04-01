67065bc983b273055c8ac2f004879934a391454c
Carlos Goce <carlosgoce@gmail.com>
2024-04-01 14:18:17 +0200

    feat: Importa rastrexos de otros años

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
   eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
-	eleventyConfig.addPassthroughCopy("assets/");
+	eleventyConfig.addPassthroughCopy("recursos/");
 	eleventyConfig.addPassthroughCopy("CNAME");
 };
