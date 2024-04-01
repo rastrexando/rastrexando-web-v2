4b18d52a927d15cf63c76abbf009b80396fb0e86
Carlos Goce <carlosgoce@gmail.com>
2024-04-02 00:36:36 +0200

    Añade plantillas de año

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias('base', 'layouts/base.njk');
   eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
+  eleventyConfig.addLayoutAlias('calendar', 'layouts/calendar.njk');
 	eleventyConfig.addPassthroughCopy("recursos/");
 	eleventyConfig.addPassthroughCopy("CNAME");
 };
