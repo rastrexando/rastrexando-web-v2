a701835d4305ad11b905cc9c37e56eedef3df12d
Carlos Goce <carlosgoce@gmail.com>
2024-04-03 22:58:16 +0200

    Mejora mucho los calendarios y la navegación de estos

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,46 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addLayoutAlias("skeleton", "layouts/skeleton.njk");
   eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
   eleventyConfig.addLayoutAlias("calendar", "layouts/calendar.njk");
   eleventyConfig.addPassthroughCopy("recursos/");
   eleventyConfig.addPassthroughCopy("CNAME");
 
+  /* Collections */
+  const now = new Date();
+
+  eleventyConfig.addCollection("upcomingPosts", function (collectionApi) {
+    return collectionApi.getAllSorted().reverse().filter(function(item) {
+      // Only posts
+      return true
+      return item.data && item.data.tags && "post" in item.data.tags;
+    }).filter(function(item) {
+      // Only upcoming
+      return item.data.date > now && item.data.tags.includes("post")
+    })
+  });
+
   /* Short codes */
+  eleventyConfig.addShortcode("renderPrevNextButtons", function (pagination, title) {
+    let prev = "<span></span>"
+    if (pagination.href.previous) {
+      prev = `<a href="${pagination.href.previous}">Anterior</a>`
+    }
+
+    let next = "<span></span>"
+    if (pagination.href.next) {
+      next =`<a href="${pagination.href.next}">Seguinte</a>`
+    }
+
+    return `
+      <div class="prev-next-buttons">
+      ${prev}
+      <h1>${title}</h1>
+      ${next}
+      </div>
+    `;
+  })
+
   eleventyConfig.addShortcode("renderPost", function (post) {
     const localeDate = post.data.date.toLocaleDateString("gl", {weekday: "long", year: "numeric", month: "long", day: "numeric"});
     let sourceHTML = ""
